4e8ded7f738dbca77681e5e95ba832838765e7a4
patriciatamega <pat.tamega@gmail.com>
2020-06-30 16:42:11 -0300

    Aspas simples e Aspas duplas

---
+++ exemplo-01.php
@@ -0,0 +1,17 @@
+<?php
+
+// Interpolação de Variáveis (um recurso do PHP)
+
+$nome = "Hcode";
+
+$nome2 = 'Treinamentos';
+
+//var_dump($nome, $nome2);
+
+echo "ABC $nome";
+
+echo "<br>";
+
+echo 'ABC $nome';
+
+?>
