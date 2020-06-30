e4b4068a4868ecdb592a2a38eb88a326307d48d3
Patrícia Tamega <45469119+patriciatamega@users.noreply.github.com>
2020-06-30 17:19:23 -0300

    Um pouco de função

---
+++ exemplo-02.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "joão rangel";
+
+// Função para deixar tudo maiúsculo
+
+echo strtoupper($nome);
+
+// Função para deixar tudo minúsculo
+
+echo strtolower($nome);
+
+// Função para deixar as iniciais do nome maiúsculas
+
+echo ucwords($nome);
+
+// Função para deixar só a primeira letra maiúscula, para um parágrafo por exemplo
+
+echo ucfirst($nome);
+
+?>
